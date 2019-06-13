6a179b364a505901c991e70abe66b529a72767fe
TinStroxGamer <38007632+TinStroxGamer@users.noreply.github.com>
2019-06-13 18:20:41 -0300

    Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,11 @@
 a = int(input("digite um numero "))
+
 b = int(input("digite outro numero "))
-print(f"a soma entre {a} e {b} é igual a {a+b}")
+#Multiplicação
+print(f"A Multiplicação entre {a} e {b} é igual a = {a * b}")
+#Subtração
+print(f"A Subtração entre {a} e {b} é igual a = {a - b}")
+#Soma
+print(f"A Soma entre {a} e {b} é igual a = {a + b}")
+#Divisão
+print(f"A Divisão entre {a} e {b} é igual a = {a / b:.2f}")
