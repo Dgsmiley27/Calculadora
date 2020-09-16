bc5fe861f42e50994bbe6b37ee4926bd9f9ae73a
Botnet1 <38007632+Dgsmiley27@users.noreply.github.com>
2020-09-16 10:40:20 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,27 +1,35 @@
 from math import sqrt
+print('####################################################')
+print('#      bem vindo a calculadora feita em python     #')
+print('#    multiplicação = 1                             #')
+print('#    subtração = 2                                 #')
+print('#    soma = 3                                      #')
+print('#    divisão = 4                                   #')
+print('#    raiz = 5                                      #')
+print('####################################################')
 calc = input("Escolha a operação = ")
 #Multiplicação
-if calc == 'multiplicação':
+if calc == '1':
     a = int(input("Escreva o primeiro numero = "))
     b = int(input("Escreva o segundo numero = "))
     print(f"A Multiplicação entre {a} e {b} é igual a = {a * b}")
 #Subtração
-elif calc == "subtração":
+elif calc == "2":
     a = int(input("Escreva o primeiro numero = "))
     b = int(input("Escreva o segundo numero = "))
     print(f"A Subtração entre {a} e {b} é igual a = {a - b}")
 #Soma
-elif calc == "soma":
+elif calc == "3":
     a = int(input("Escreva o primeiro numero = "))
     b = int(input("Escreva o segundo numero = "))
     print(f"A Soma entre {a} e {b} é igual a = {a + b}")
 #Divisão
-elif calc == 'divisão':
+elif calc == '4':
     a = int(input("Escreva o primeiro numero = "))
     b = int(input("Escreva o segundo numero = "))
     print(f"A Divisão entre {a} e {b} é igual a = {a / b:.2f}")
 #Raiz
-elif calc =='raiz':
+elif calc =='5':
     a = int(input("Escreva o numero = "))
     print(f"a raiz de {a} é igual a {sqrt(a)}")
 else:
